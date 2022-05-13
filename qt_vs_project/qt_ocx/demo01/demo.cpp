e801fc5e8c7f10d59645e2972b5bd0477bebec8f
Your Name <zhangchangbo@linxrobot.com>
2022-05-13 18:13:00 +0800

    qt嵌入ocx

---
+++ qt_vs_project/qt_ocx/demo01/demo.cpp
@@ -0,0 +1,15 @@
+﻿#include "demo.h"
+
+Demo::Demo(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	xgx = new XGXLib::XGX(ui.centralWidget);
+}
+
+// slot 
+void Demo::on_pushButton_clicked()
+{
+	xgx->AboutBox();
+}
+
